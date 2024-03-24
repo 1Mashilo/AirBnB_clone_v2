4f19060780f1275cc2e5fadb15e16b79f6e9164d
1Mashilo <christophermashilo@gmail.com>
2024-03-24 16:37:26 +0200

    submit task

--- models/city.py
+++ models/city.py
@@ -10,7 +10,10 @@ Attributes:
 """
 
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class City(BaseModel, Base):
@@ -26,3 +29,5 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
