64b9bf53c8a48dd90aa90ad068e730b0cf90f5a2
1Mashilo <christophermashilo@gmail.com>
2024-03-25 19:07:16 +0200

    changed file permission

--- models/city.py
+++ models/city.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
-
-"""
-This module defines the City class, representing a geographical city.
-The City class inherits from the BaseModel class.
-
-Attributes:
-    state_id (str): ID of the state the city is located in.
-    name (str): Name of the city.
-"""
-
+"""This is the city class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import ForeignKey
@@ -17,16 +9,12 @@ from models.place import Place
 
 
 class City(BaseModel, Base):
-    """
-    Represents a City for the AirBnB clone project.
-
+    """This is the class for City
     Attributes:
-        __tablename__ (str): Name of the table in the database.
-        name (str): Name of the city.
-        state_id (str): ID of the state the city is located in.
+        state_id: The state id
+        name: input name
     """
-
-    __tablename__ = 'cities'
+    __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     places = relationship("Place", cascade='all, delete, delete-orphan',
